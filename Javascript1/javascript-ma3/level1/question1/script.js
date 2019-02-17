56b1f2e466cc29390315febf46fd16fbb46aac46
lassespilling <lassespilling@gmail.com>
2019-02-17 16:51:38 +0100

    Delivery

--- Javascript1/javascript-ma3/level1/question1/script.js
+++ Javascript1/javascript-ma3/level1/question1/script.js
@@ -1,1 +1,61 @@
-//Use RegEx to validate form
+// Variables
+var fne = document.getElementById("first-name-errors");
+var lne = document.getElementById("last-name-errors");
+var pe = document.getElementById("phone-errors");
+var ee = document.getElementById("email-errors");
+var li = document.getElementsByTagName("li");
+var fname;
+var lname;
+
+// RegEx to validate, phone allows whitespace
+var isText = /[a-zA-Z]/;
+var isPhone = /\+\s*?\d\s*?\d\s*?\d\s*?\d\s*?\d\s*?\d\s*?\d\s*?\d\s*?\d\s*?\d\s*?/;
+var hasAreaCode = /^(\s?\+\s?\d\s?\d\s?)/;
+var hasAt = /@/;
+var hasDot = /\./;
+var isEmail = /[a-zA-Z0-9\_\-\.]@[a-zA-Z0-9\_\-\.]\.[a-zA-Z0-9]/;
+
+function validate() {
+    // Get updated values on button press
+    var fname = document.getElementById("first").value;
+    var lname = document.getElementById("last").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    
+    // First name
+    if(fname.length === 0) { // If 0 characters error
+        fne.innerHTML = "<li>First name is missing</li>";
+    } else if(!isText.test(fname)) { // If not text error
+        fne.innerHTML = "<li>First name can only be letters a-z.</li>";
+    } else { // Reset to 0 for next button press
+        fne.innerHTML = "";
+    }
+    // Last name
+    if(lname.length === 0) { // If 0 characters error
+        lne.innerHTML = "<li>Last name is missing</li>";
+    } else if(!isText.test(lname)) { // If not text error
+        lne.innerHTML = "<li>Last name can only contain letters from a-z.</li>";
+    } else { // Reset to 0 for next button press
+        lne.innerHTML = "";
+    }
+    // Phone number
+    if(phone.length === 0) { // If 0 characters error
+        pe.innerHTML = "<li>Phone number is missing.</li>";
+    } else if(!hasAreaCode.test(phone)) { // If not text error
+        pe.innerHTML = "<li>Phone number is missing area code.</li>";
+    } else if(!isPhone.test(phone)) { // If not text error
+        pe.innerHTML = "<li>Phone number is flawed.</li>";
+    } else { // Reset to 0 for next button press
+        pe.innerHTML = "";
+    }
+    // E-mail
+    if(email.length === 0) { // If 0 characters error
+        ee.innerHTML = "<li>E-mail is missing.</li>";
+    } else if(!hasAt.test(email)) { // If missing @
+        pe.innerHTML = "<li>Email is missing a @</li>";
+    } else if(!hasDot.test(email)) { // If missing .
+        pe.innerHTML = "<li>Email is missing a .</li>";
+    } else { // Reset to 0 for next button press
+        ee.innerHTML = "";
+    }
+}
--- Javascript1/javascript-ma3/level1/question2/script.js
+++ Javascript1/javascript-ma3/level1/question2/script.js
@@ -1,1 +1,40 @@
 //Console.log out elements in JSON file
+var jsonData = {
+	"video": [{
+		"id": 12312412312,
+		"name": "Ecuaciones Diferenciales",
+		"url": "/video/math/edo/12312412312",
+		"author": {
+			"data": [{
+				"name_author": "Alejandro Morales",
+				"url": "/author/alejandro-morales",
+				"type": "master"
+			}]
+		}
+	}]
+};
+var array = jsonData.video;
+var nestedarray = array[0].author.data;
+
+// using for loop
+for (var i = 0;i<array.length; i++) {
+    console.log(array[i]);
+}
+
+/* optional second loop for nested array "author.data" 
+for (var i = 0;i<nestedarray.length; i++) {
+    console.log(nestedarray[i]);
+}
+*/
+
+/* individually
+console.log(array[0]);
+console.log(array[0].id);
+console.log(array[0].name);
+console.log(array[0].url);
+console.log(array[0].author);
+console.log(array[0].author.data[0]);
+console.log(array[0].author.data[0].name_author);
+console.log(array[0].author.data[0].url);
+console.log(array[0].author.data[0].type);
+*/
